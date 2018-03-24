9f0dfe160ae13760a83c515df1c70a800bc434ed
Coty Hamilton <cotyhamilton@gmail.com>
2018-03-24 10:16:57 -0500

    updated api key with hh100mobile account

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,8 +17,15 @@ export class MyApp {
       //apiKey: "AIzaSyDmoyHBk0bekmSUqW4qL9G7x2Gh9CvlA6w",
       //authDomain: "hh100mobile-21dce.firebaseapp.com",
 
-      apiKey: "AIzaSyDhQk9JWDz-RiT45tlKkzCTbkN5H8YU-FM",
-      authDomain: "hh100-a8856.firebaseapp.com",
+      //coty firebase
+      // apiKey: "AIzaSyDhQk9JWDz-RiT45tlKkzCTbkN5H8YU-FM",
+      // authDomain: "hh100-a8856.firebaseapp.com",
+
+      //hh100 firebase
+      apiKey: "AIzaSyB5fBejTe-iUZkGAQTkpjS3tG0d1N9-QSs",
+      authDomain: "hh100-98a6c.firebaseapp.com",
+
+
     });
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
